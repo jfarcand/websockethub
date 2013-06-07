6e7f9de11c93febb55d5ae5c168aa9b317336196
jfarcand <jfarcand@apache.org>
2013-06-07 14:11:22 -0400

    Use ID instead of path

--- src/main/java/org/atmosphere/samples/rabbitmq/WebSocketHub.java
+++ src/main/java/org/atmosphere/samples/rabbitmq/WebSocketHub.java
@@ -16,10 +16,13 @@
 package org.atmosphere.samples.rabbitmq;
 
 import org.atmosphere.cache.UUIDBroadcasterCache;
+import org.atmosphere.config.service.Singleton;
 import org.atmosphere.config.service.WebSocketHandlerService;
 import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.BroadcasterFactory;
 import org.atmosphere.interceptor.AtmosphereResourceStateRecovery;
 import org.atmosphere.interceptor.HeartbeatInterceptor;
+import org.atmosphere.util.SimpleBroadcaster;
 import org.atmosphere.websocket.WebSocket;
 import org.atmosphere.websocket.WebSocketHandlerAdapter;
 import org.atmosphere.websocket.WebSocketProcessor;
@@ -28,32 +31,57 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+@Singleton
 @WebSocketHandlerService(
+        broadcaster = SimpleBroadcaster.class,
         broadcasterCache = UUIDBroadcasterCache.class,
         interceptors = {AtmosphereResourceStateRecovery.class, HeartbeatInterceptor.class},
-        path = "/{email}")
+        path = "/")
 public class WebSocketHub extends WebSocketHandlerAdapter {
 
     private final Logger logger = LoggerFactory.getLogger(WebSocketHub.class);
     private RabbitMQRouter router;
-    private String routingKey;
+    private final static String ROUTING_KEY = "id";
 
     @Override
     public void onTextMessage(final WebSocket webSocket, String data) throws IOException {
-        router.deliver(routingKey, data);
+        router.deliver(routingKey(webSocket), data);
     }
 
     @Override
     public void onOpen(final WebSocket webSocket) throws IOException {
         AtmosphereResource r = webSocket.resource();
         router = RabbitMQRouter.createOrGet(r.getAtmosphereConfig());
-        routingKey = router.register(r.getBroadcaster());
+
+        String routingKey = routingKey(webSocket);
+        if (routingKey == null) {
+            logger.error("Routing Key is null for {}", r.uuid());
+            webSocket.close();
+            return;
+        }
+
+        //Sanity check to make sure nobody is hacking the routingKey
+        BroadcasterFactory f = r.getAtmosphereConfig().getBroadcasterFactory();
+        try {
+            f.get(routingKey);
+        } catch (IllegalStateException ex) {
+            logger.error("Routing Key is used for {}", routingKey);
+            webSocket.close();
+            return;
+        }
+        f.lookup(routingKey, true).addAtmosphereResource(r);
+        router.register(r.getBroadcaster());
+    }
+
+    String routingKey(WebSocket webSocket) {
+        AtmosphereResource r = webSocket.resource();
+        return "/" + r.getRequest().getParameter(ROUTING_KEY);
     }
 
     @Override
     public void onClose(WebSocket webSocket) {
         if (router != null) {
-            router.unregister(routingKey);
+            router.unregister(routingKey(webSocket));
         }
     }
 
